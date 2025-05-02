7c79c372d4bcbded8c529964b21dfc653a52225f
seveneleven711thanks39 <160039639+seveneleven711thanks39@users.noreply.github.com>
2025-05-03 08:56:30 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
 # coding: utf-8
-
 import pandas as pd
 import os
 
+
 #Load data from an Excel file using Pandas
-MYDIR = "Your directory"
-filename = os.path.join(MYDIR, 'file name')
+MYDIR = "./"
+filename = os.path.join(MYDIR, 'sample_data.xlsx')
 
 df = pd.read_excel(filename,
-                   sheet_name="Sheet1",
-                  engine='openpyxl',)
+               sheet_name="Sheet1",
+              engine='openpyxl',)
