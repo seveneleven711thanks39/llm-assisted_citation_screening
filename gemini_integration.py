8c252b3de04e81b19a6c90998944c2ac7de76203
seveneleven711thanks39 <160039639+seveneleven711thanks39@users.noreply.github.com>
2025-05-03 09:04:44 +0900

    Update gemini_integration.py

--- gemini_integration.py
+++ gemini_integration.py
@@ -5,51 +5,47 @@
 import google.generativeai as genai
 import openpyxl
 
+def runGemini():
 # Set the Gemini 1.5 Pro API key
-genai.configure(api_key="Your api_key")
-
+    genai.configure(api_key="xxxxxxxxxxxxxxxxxxxxxxxx")
 # Set the safety settings
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE"
-    }
-]
-
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE"
+        }
+    ]
 # Set the model
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest", safety_settings=safety_settings)
-
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash-8b", safety_settings=safety_settings)
 # Read an Excel file
-workbook = openpyxl.load_workbook("Path to your file")
-sheet = workbook.active
-
+    workbook = openpyxl.load_workbook("sample_data.xlsx")
+    sheet = workbook.active
 # Apply the range of the row and column
-start_row = "number"
-end_row = "number"
-question_column = "number"
-answer_column = "number"
-
+    start_row = 1
+    end_row = 15
+    question_column = 23
+    answer_column = 24
 # Repeat the execution of the command 
-for row in range(start_row, end_row + 1):
-    question = sheet.cell(row=row, column=question_column).value
-    if question:
-        # Answer the question using the Gemini 1.5 Pro API
-        response = model.generate_content(question)
-        answer = response.text
-        # Write an answer next to the cell
-        sheet.cell(row=row, column=answer_column).value = answer
-
+    for row in range(start_row, end_row + 1):
+        print(row)
+        question = sheet.cell(row=row, column=question_column).value
+        if question:
+            # Answer the question using the Gemini 1.5 Pro API
+            response = model.generate_content(question)
+            answer = response.text
+            # Write an answer next to the cell
+            sheet.cell(row=row, column=answer_column).value = answer
 # Save the edited Excel file
-workbook.save("Path to your file")
+    workbook.save("sample_data.xlsx")
